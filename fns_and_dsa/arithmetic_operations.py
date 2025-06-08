9d6b28eb876c524173567d89bdbbf6165f7b8361
Bereket Weldecherkos <bereketweldecherkos@gmail.com>
2025-06-08 11:44:24 +0300

    FIX arithmetic_operations.py 3

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -23,4 +23,3 @@ def perform_operation(num1, num2, operation):
     else:
         return "Invalid operation"
     
-    /tmp/correction/7043434573485684130543536601201742592992_5/100741/657602/fns_and_dsa/arithmetic_operations.py doesn't contain num2\s==\s0:
