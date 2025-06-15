80eb0b8411525fae89e528602176e945ed648370
Bereket Weldecherkos <bereketweldecherkos@gmail.com>
2025-06-15 11:11:24 +0300

    Add bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount():
+    def __init__(self, account_balance =0):
+        self.account_balance  = account_balance 
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance  += amount
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance :
+            self.account_balance  -= amount
+            return True
+        return False
+
+    def display_balance (self):
+        print(f"Current Balance: ${self.account_balance }")
+        # return self.account_balance 
+
+    def __str__(self):
+        return f"Account Number: {self.account_number}, Balance: {self.account_balance }"
